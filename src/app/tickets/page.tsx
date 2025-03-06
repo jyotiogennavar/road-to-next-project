fadc5b5f38e4dae1fa52c7d9b9ae3fbe1ae88a80
Jyoti <jyoti.ogennavar@xecurify.com>
2025-03-06 11:46:58 +0530

    Remove ErrorBoundary from TicketsPage and adjust component imports for cleaner structure

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,5 @@
 import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { Header } from "@/components/Header";
-import { Placeholder } from "@/components/PlaceHolder";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
@@ -10,11 +8,11 @@ const TicketsPage = () => {
     <div className="flex-1 flex flex-col gap-y-4">
       <Header title="Tickets" subtitle="Manage your tickets here" />
 
-      <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
+
         <Suspense fallback={<Spinner />}>
           <TicketList />
         </Suspense>
-      </ErrorBoundary>
+
     </div>
   );
 };
